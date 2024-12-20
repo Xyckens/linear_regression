c46f7b42957a7c6f7c56ed00152e8a78290168e7
Francisco Vieira <fvieira@c1r15s6.42porto.com>
2024-12-20 19:06:04 +0000

    refactored everything into its own function, added gitignore for data.csv

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -3,23 +3,26 @@ import numpy as np
 
 theta0 = 12.715
 theta1 = 17.615000000000002
+data_len = 0
+mileage = []
+price = []
 
 def estimate(mileage, theta0, theta1):
     return theta0 + theta1 * mileage
 
-def train(dataset):
-    mileage = []
-    price = []
-    counter = 0
+def read_data(dataset):
+    global data_len
+    global mileage
+    global price
     with open(dataset) as csvfile:
         rows = csv.reader(csvfile)
         next(rows)
         for row in rows:
             mileage.append(row[0])
             price.append(row[1])
-            counter += 1
+            data_len += 1
     
-def change_thetas():
+def change_thetas(final_theta0, final_theta1):
     with open(__file__, 'r') as f:
         lines = f.read().split('\n')
         val_theta0 = float(lines[3].split('=')[-1])
@@ -40,6 +43,21 @@ def reset():
     
     with open(__file__, 'w') as f:
         f.write(new_file)
+
+def train(dataset, learningRate):
+    global  data_len
+    global  mileage
+    global  price
+    i = 0
+    const1 = learningRate * (1 / data_len)
+    while (i <= data_len - 1):
+        est = estimate(theta0, theta1, mileage[i] - price[i])
+        theta0 += est
+        theta1 += est * mileage[i]
+        i += 1
+    theta0 = const1 * theta0
+    theta1 = const1 * theta1
+    change_thetas(theta0, theta1)
 
 if __name__ == "__main__":
     train("data.csv")
