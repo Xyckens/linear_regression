b05d9f60d87b085b473e82fb679a2e50c2a69b67
Francisco Vieira <fvieira@c1r15s6.42porto.com>
2025-01-05 19:25:00 +0000

    better but why

--- ft_linear_regression.py
+++ ft_linear_regression.py
@@ -13,6 +13,8 @@ class   linear_regression:
     fig, ax = plt.subplots()
     x = np.linspace(-2 , 3, 100)
     line, = ax.plot(x, 0 * x)
+    theta0 = 0
+    theta1 = 0
 
     def __init__(self, dataset) -> None:
         self.dataset = dataset
@@ -25,7 +27,7 @@ class   linear_regression:
                     self.price.append(int(row[1]))
                     self.data_len += 1
             self.mileage = (self.mileage - np.mean(self.mileage)) / np.std(self.mileage)
-            self.price = (self.price - np.mean(self.price)) / np.std(self.price)
+            #self.price = (self.price - np.mean(self.price)) / np.std(self.price)
             plt.ion() #interactive mode
             plt.plot(self.mileage, self.price, 'r.')
             plt.ylabel("Pricing")
@@ -54,19 +56,19 @@ class   linear_regression:
         self.change_thetas(0, 0)
 
     #explica learning rate hyperparameter
-    #mas basicamente, pequeno de mais e demora muito a convergir,
-    # grande demais e nunca converge
+    #basicamente, pequeno de mais e demora muito a convergir,
+    #grande demais e nunca converge
     #https://developers.google.com/machine-learning/crash-course/linear-regression/hyperparameters
 
-    def estimate(self, theta0, theta1, mileage) -> float:
-        return theta0 + theta1 * mileage
+    def estimate(self, b, m, x) -> float:
+        return b + m * x
 
-    def train(self, learningRate, theta0, theta1) -> tuple[float, float]:
+    def train(self, learningRate) -> tuple[float, float]:
         i = 0
         sum0 = 0
         sum1 = 0
         while (i <= self.data_len - 1):
-            est = self.estimate(theta0, theta1, self.mileage[i]/100000) - self.price[i] / 1000
+            est = self.estimate(self.theta0, self.theta1, self.mileage[i]) - self.price[i]
             #print(f"est =  {est:.2e}")
             sum0 += est
             sum1 += est * self.mileage[i]
@@ -75,46 +77,46 @@ class   linear_regression:
         return const * sum0, const * sum1
 
     def find_learning_rate(self) -> None:
-        global  theta0
-        global  theta1
         delta0 = 11
         delta1 = 11
         learningRate = 0.01
         iteration = 0
-        while (iteration < 30):
-            new0, new1 = self.train(learningRate, theta0, theta1)
-            delta0 = abs(theta0 - new0)
-            delta1 = abs(theta1 - new1)
+        #while (iteration < 100):
+        while (delta0 != 0 and delta1 != 0):
+            new0, new1 = self.train(learningRate)
+            delta0 = abs(self.theta0 - new0)
+            delta1 = abs(self.theta1 - new1)
             print(f"new0 {new0:.03e} and new1 {new1:.03e} delta0 {delta0:.03e} delta1 {delta1:.03e}")
-            theta0 = new0
-            theta1 = new1
+            self.theta0 -= new0
+            self.theta1 -= new1
             iteration += 1
             '''
             if (delta0 > 1000000000 or delta1 > 1000000000):
                 print("delta got too high")
                 break
             '''
-            self.data_graph(theta0, theta1)
+            self.data_graph(self.theta0, self.theta1)
         m,b = np.polyfit(np.array(self.mileage), np.array(self.price), 1)
-        print(m, b)
+        #correct value
+        print(b, m)
         plt.plot(self.x, b + m * self.x, "g")
         print("iterations = ", iteration)
         plt.ioff()
         plt.show()
 
     #bonus   
-    def data_graph(self, theta0, theta1) -> None:
-        y = theta0 + self.x * theta1
+    def data_graph(self, b, m) -> None:
+        y = b + self.x * m
         self.line.set_ydata(y)
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
-        plt.pause(0.1)
+        plt.pause(0.05)
 
-    def precision(self,theta0, theta1) -> float: #mean absolute error
+    def precision(self, b, m) -> float: #mean absolute error
         i = 0
         est = 0
         while (i <= self.data_len - 1):
-            est += abs(self.estimate(theta0, theta1, self.mileage[i]) - self.price[i])
+            est += abs(self.estimate(b, m, self.mileage[i]) - self.price[i])
             i += 1
         return est / self.data_len
     
